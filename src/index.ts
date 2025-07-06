29ad2041914d7b6a1b31b2927963c7d92d8a27c9
Mike Bannister <notimpossiblemike@gmail.com>
2025-07-06 02:52:23 -0400

    Handle commander version command exit gracefully
    Add commander.version to error codes that trigger clean exit, preventing error display when users run --version flag.

--- src/index.ts
+++ src/index.ts
@@ -33,7 +33,8 @@ async function main() {
   } catch (error: any) {
     if (
       error.code === 'commander.help' ||
-      error.code === 'commander.helpDisplayed'
+      error.code === 'commander.helpDisplayed' ||
+      error.code === 'commander.version'
     ) {
       process.exit(0)
     }
